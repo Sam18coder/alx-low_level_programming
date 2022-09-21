59a789b97dcaaf93c5818f79a604eb296240ff78
Sam18coder <105773535+Sam18coder@users.noreply.github.com>
2022-09-22 00:14:29 +0100

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,7 +10,7 @@ void reverse_array(int *a, int n);
 void print_number(int n);
 void print_buffer(char *b, int size);
 int _strcmp(char *sl, char *s2);
-char *strncat(char *dest, char *src);
+char *strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 char *string_toupper(char *);
